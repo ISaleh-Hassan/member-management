60ecb956944c7ea8613f056a87564acad7617a9f
Saleh Hassan <saleh.hassan@volvo.com>
2022-06-18 02:38:44 +0200

    feat: edit docker-compose.yml

--- backend/src/main/java/com/sweden/association/membermanagement/MemberManagementApplication.java
+++ backend/src/main/java/com/sweden/association/membermanagement/MemberManagementApplication.java
@@ -2,10 +2,8 @@ package com.sweden.association.membermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.profiles.active:prod}.properties")
 public class MemberManagementApplication {
 
 	public static void main(String[] args) {
