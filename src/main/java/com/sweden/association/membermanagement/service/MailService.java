fb27b95433f136a91cc54e1ac2c415eff961fd57
Saleh Hassan <saleh.hassan@volvo.com>
2022-07-22 21:21:04 +0200

    feat: Move some hardcoded thing to a config file

--- src/main/java/com/sweden/association/membermanagement/service/MailService.java
+++ src/main/java/com/sweden/association/membermanagement/service/MailService.java
@@ -1,6 +1,7 @@
 package com.sweden.association.membermanagement.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,11 +14,14 @@ public class MailService {
 
   @Autowired
   private JavaMailSender mailSender;
+  @Autowired
+  private Environment env;
 
   protected void sendConfirmRegistration(UserAccount userAccount) throws MailException {
     String subject = "Registration Confirmation";
-    String confirmationUrl = "http://localhost:8083/api/v1/user-accounts/registrationConfirm?token="
-        + userAccount.getVerificationToken();
+    final String baseUrl = env.getProperty("base.url");
+
+    String confirmationUrl = baseUrl + "/api/v1/user-accounts/registrationConfirm?token="+ userAccount.getVerificationToken();
     String message = "Hi " + userAccount.getMemberUserAccount().getFirstName() + " please click this link to verify your email address needed for accessing the member management application";
     SimpleMailMessage email = new SimpleMailMessage();
     email.setTo(userAccount.getEmail());
@@ -28,13 +32,16 @@ public class MailService {
 
   public void sendGrantUserToAdmin(UserAccount userAccount) throws MailException {
     String subject = "Grant admin priviliges";
-    String grantAdminUrl = "http://localhost:8083/api/v1/user-accounts/grantAdminRights?id="
+    final String baseUrl = env.getProperty("base.url");
+    final String adminEmail = env.getProperty("admin.email");
+
+    String grantAdminUrl = baseUrl + "/api/v1/user-accounts/grantAdminRights?id="
         + userAccount.getUserAccountId();
     String message = "The user " + userAccount.getMemberUserAccount().getFirstName()
         + " has just been verified please grant him or her admin priviliges in the membermanagement application" + "\r\n" +  grantAdminUrl;
 
     SimpleMailMessage email = new SimpleMailMessage();
-    email.setTo("carl.hillman@gmail.com");// here we set the mail of the admin who grants admin priviliges to users
+    email.setTo(adminEmail);// here we set the mail of the admin who grants admin priviliges to users
     email.setSubject(subject);
     email.setText(message);
     mailSender.send(email);
